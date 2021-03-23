42d9185a65c8b5bc240311200d29bfaf7966a004
SEONGBIN-PYO <binch3627@gmail.com>
2021-03-23 23:43:45 +0900

    chapter18 학습

--- chapter18/elementStyling.js
+++ chapter18/elementStyling.js
@@ -4,8 +4,35 @@ function highlightParas(containing) {
     const paras = document.getElementsByTagName('p');
     console.log(paras);
     for(let p of paras) {
-        if(!containing.test(p.textContent)) continue;
+        //console.log(p.textContent);
+        if(!containing.test(p.textContent)) {
+            console.log(containing.test(p.textContent));
+            continue;
+        }
         p.classList.add('highlight');
     }
 }
-highlightParas('unique');
+
+function removeParaHighlights() {
+    const paras = document.querySelectorAll('p.highlight');
+    for(let p of paras) {
+        p.classList.remove('highlight')
+    }
+}
+
+const highlightActions = document.querySelectorAll('[data-action="highlight"]');
+for(let a of highlightActions) {
+    a.addEventListener('click', evt => {
+        evt.preventDefault();
+        highlightParas(a.dataset.containing);
+    });
+}
+
+const removeHighlightActions = document.querySelectorAll('[data-action="removeHighlights"]');
+
+for(let a of removeHighlightActions) {
+    a.addEventListener('click', evt => {
+        evt.preventDefault();
+        removeParaHighlights();
+    });
+}
